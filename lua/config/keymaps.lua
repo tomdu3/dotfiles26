6480d3b2877104ae2660472f6eef252c927e29f6
Tomislav Dukez <tomdu3@ymail.com>
2026-02-13 12:33:14 +0000

    add keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,79 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+--local discipline = require("craftzdog.discipline")
+--discipline.cowboy()
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backwards
+keymap.set("n", "dw", "vb_d")
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Jumplist
+keymap.set("n", "<C-m>", "<C-i>", opts)
+
+-- Disable continuations
+-- keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
+-- keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
+
+-- New tab
+keymap.set("n", "te", ":tabedit")
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+
+-- Split window
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+-- Move window
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+
+-- Resize window
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><>up", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
+
+-- Diagnostics
+keymap.set("n", "<C-j>", function()
+  vim.diagnostic.goto_next()
+end, opts)
+
+-- Map 'jk' to behave like Ctrl-C in Normal mode
+keymap.set("i", "jk", "<Esc>", opts)
+keymap.set("i", "kj", "<Esc>", opts)
+
+-- telescope
+vim.keymap.set("n", "<leader>fs", ":Telescope find_files<cr>")
+vim.keymap.set("n", "<leader>fp", ":Telescope git_files<cr>")
+vim.keymap.set("n", "<leader>fz", ":Telescope live_grep<cr>")
+vim.keymap.set("n", "<leader>fo", ":Telescope oldfiles<cr>")
+vim.keymap.set("n", "<leader>fb", ":Telescope buffers<cr>")
+vim.keymap.set("n", "<leader>fh", ":Telescope help_tags<cr>")
+
+-- icon picker
+vim.keymap.set("n", "<leader>ic", ":IconPickerNormal<cr>", { noremap = true, silent = true })
+
+-- zen mode
+vim.keymap.set("n", "<leader>zm", ":ZenMode<cr>")
+
+-- format code using LSP
+-- vim.keymap.set("n", "<leader>pp", vim.lsp.buf.format)
+
+-- markdown preview
+vim.keymap.set("n", "<leader>mp", ":MarkdownPreviewToggle<cr>")
+
+-- nvim-comment
+vim.keymap.set({ "n", "v" }, "<leader>/", ":CommentToggle<cr>")
