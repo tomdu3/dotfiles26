eaecc59d8f3cdcaf022d8c9c3918be3ee8fc7008
Tomislav Dukez <tomdu3@ymail.com>
2026-03-16 18:03:41 +0000

    update options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.opt.fileencodings = "utf-8,gbk,gb18030,latin1"
+vim.opt.encoding = "utf-8"
+vim.g.lazyvim_nerd_font = true
+
+-- Set guifont explicitly
+vim.o.guifont = "Hack Nerd Font Mono:h12"
